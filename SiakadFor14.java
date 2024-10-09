615ed56ae2f78c644d2fd2d1be974d5891848516
Aldyth11 <aldyth.r.f@gmail.com>
2024-10-09 12:52:46 +0700

    Percobaan 1 no 4

--- SiakadFor14.java
+++ SiakadFor14.java
@@ -4,6 +4,7 @@ public class SiakadFor14 {
     public static void main(String[] args) {
         Scanner input14 = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int mhsLulus = 0, mhsTdklulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -13,10 +14,15 @@ public class SiakadFor14 {
             }
             if (nilai < terendah) {
                 terendah = nilai;
-            }
+            } else if (nilai >= 60) {
+                mhsLulus++;
+            } else 
+                mhsTdklulus++;
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + mhsLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + mhsTdklulus);
     }
 
 }
